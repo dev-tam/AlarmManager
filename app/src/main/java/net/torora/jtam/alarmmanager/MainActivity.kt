d0ead734bcd312c509039a25a8ce01173744ba2c
dev-tam <tamjid.mdh@gmail.com>
2017-12-17 20:52:54 +0600

    Initial commit

---
+++ app/src/main/java/net/torora/jtam/alarmmanager/MainActivity.kt
@@ -0,0 +1,17 @@
+package net.torora.jtam.alarmmanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun BuSetTime(view: View){
+
+    }
+}
